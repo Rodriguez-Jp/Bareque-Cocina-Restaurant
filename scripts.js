b0d42ce238037508acd28ed8d7b5f61d7ffa0997
houdinibeanie <jaybritany@gmail.com>
2021-11-15 13:35:00 -0500

    Commenting Code 1

--- scripts.js
+++ scripts.js
@@ -1,19 +1,20 @@
-// Hamburger Menu
-const navMenuIcon1 = $("#bars-link");
-const navMenuIcon2 = $(".fa-grip-lines");
-const pageLinkMenu = $("#nav-links");
+// HAMBURGER MENU //
+const navMenuIcon1 = $("#bars-link"); //triple bar menu icon
+const navMenuIcon2 = $(".fa-grip-lines"); //double bar menu icon
+const pageLinkMenu = $("#nav-links"); //menu links (i.e. Menu, Contact, Home)
 
+//function attached to 'click' event listener for mobile menu dropdown (click event applied in html)
 function mobileMenuClick1() {
-    navMenuIcon2.slideDown();
-    navMenuIcon1.css("display", "none");
-    pageLinkMenu.slideDown();
+    navMenuIcon2.slideDown(); //appear double line menu icon
+    navMenuIcon1.css("display", "none");//hide triple line menu icon
+    pageLinkMenu.slideDown();//slide down menu links from navabr
 };
 function mobileMenuClick2() {
-  navMenuIcon2.css("display", "none");
-  navMenuIcon1.slideDown();
-  pageLinkMenu.slideUp();
+  navMenuIcon2.css("display", "none");//hide double line menu icon
+  navMenuIcon1.slideDown();//appear triple line menu icon
+  pageLinkMenu.slideUp();//hide menu links dropdown
 };
-
+//function for mouseover event listener for menu icons
 function mobileMenuMouseover() {
 $(".fa-bars").css("color","#BBA484");
 $(".fa-grip-lines").css("color","#BBA484");
